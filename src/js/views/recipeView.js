2673da3739f395a50c9a045cdda76764f365f0cc
Ekremina <ekreminaarik@gmail.com>
2021-07-04 02:31:23 +0100

    Helpers and configuration files

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,5 +1,5 @@
-import icons from '../../img/icons.svg';
-// import icons from 'url:../../img/icons.svg';
+// import icons from '../../img/icons.svg';
+import icons from 'url:../../img/icons.svg'; // Parcel 2
 import { Fraction } from 'fractional';
 
 class RecipeView {
