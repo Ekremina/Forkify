752c982111173ea61df65393d47cc24ccf71ad93
Ekremina <ekreminaarik@gmail.com>
2021-07-13 22:04:26 +0100

    Store bookmarks with localStorage

--- src/js/views/bookmarksView.js
+++ src/js/views/bookmarksView.js
@@ -5,9 +5,13 @@ import icons from '../../img/icons.svg';
 class BookmarksView extends View {
   _parentElement = document.querySelector('.bookmarks__list');
   _errorMessage = 'No bookmarks yet. Find a recipe and bookmark it!';
+  _message = '';
+
+  addHandlerRender(handler) {
+    window.addEventListener('load', handler);
+  }
 
   _generateMarkup() {
-    console.log(this._data);
     return this._data
       .map(bookmark => previewView.render(bookmark, false))
       .join('');
