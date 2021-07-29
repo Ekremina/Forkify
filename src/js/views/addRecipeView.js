85b3380cb32760a3a43755ce207467da0ee07570
Ekremina <ekreminaarik@gmail.com>
2021-07-29 21:35:26 +0100

    add a new recipe

--- src/js/views/addRecipeView.js
+++ src/js/views/addRecipeView.js
@@ -12,15 +12,30 @@ class AddRecipeView extends View {
   constructor() {
     super();
     this._addHandlerShowWindow();
+    this._addHandlerHideWindow();
   }
 
+  toggleWindow() {
+    this._overlay.classList.toggle('hidden');
+    this._window.classList.toggle('hidden');
+  }
   _addHandlerShowWindow() {
-    this._btnOpen.addEventListener('click', function () {
-      this._overlay.classList.toggle('hidden');
-      this._window.classList.toggle('hidden');
+    this._btnOpen.addEventListener('click', this.toggleWindow.bind(this));
+  }
+
+  _addHandlerHideWindow() {
+    this._btnClose.addEventListener('click', this.toggleWindow.bind(this));
+    this._overlay.addEventListener('click', this.toggleWindow.bind(this));
+  }
+
+  addHandlerUpload(handler) {
+    this._parentElement.addEventListener('submit', function () {
+      e.preventDefault();
+      const dataArr = [...new FormData(this)];
+      const data = Object.fromEntries(dataArr);
+      handler(data);
     });
   }
-
   _generateMarkup() {}
 }
 
