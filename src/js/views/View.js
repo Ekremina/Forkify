30ab83de7509dfbdc9389c821bf74c137402d0c8
Ekremina <ekreminaarik@gmail.com>
2021-08-18 22:28:20 +0100

    add jsdoc format

--- src/js/views/View.js
+++ src/js/views/View.js
@@ -1,8 +1,13 @@
-import icons from '../../img/icons.svg';
-// import icons from 'url:../../img/icons.svg'; // Parcel 2
+import icons from 'url:../../img/icons.svg'; // Parcel 2
 
 export default class View {
   _data;
+  /**
+   * Render the received object to the DOM
+   * @param {Object | object[]} data The data to be rendered (e.g. recipe)
+   * @param {boolean} [render=true] If false, create markup string instead of rendering to the DOM
+   * @returns
+   */
 
   render(data, render = true) {
     if (!data || (Array.isArray(data) && data.length === 0))
